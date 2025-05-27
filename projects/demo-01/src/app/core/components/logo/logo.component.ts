83032a05b107cfc7128078e263cb4e72a2329897
Alejandro Cerezo <alce65@hotmail.es>
2025-05-27 19:24:20 +0200

    Change favicon. Add Logo Component

---
+++ projects/demo-01/src/app/core/components/logo/logo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ine-logo',
+  imports: [],
+  templateUrl: './logo.component.svg',
+  // template: '',
+  styles: ``,
+})
+export class LogoComponent {
+  size = '2.5rem';
+  upperColor = '#a52a2a';
+  downColor = '#666';
+}
