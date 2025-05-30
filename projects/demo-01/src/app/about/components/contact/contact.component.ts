6aa93a5d4a8df65e9116c18da215b059b512013c
Alejandro Cerezo <alce65@hotmail.es>
2025-05-30 09:23:11 +0200

    Update Contact form with error messages

--- projects/demo-01/src/app/about/components/contact/contact.component.ts
+++ projects/demo-01/src/app/about/components/contact/contact.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { UserDTO } from '../../types.ts/user';
 
 @Component({
@@ -11,10 +16,34 @@ import { UserDTO } from '../../types.ts/user';
         Nombre:
         <input type="text" formControlName="name" />
       </label>
+      @if (
+        formGroupContact.get('name')?.invalid &&
+        formGroupContact.get('name')?.touched
+      ) {
+        <div>
+          @if (formGroupContact.get('name')?.errors?.['required']) {
+            <span>El nombre es requerido.</span>
+          } @else if (formGroupContact.get('name')?.errors?.['minlength']) {
+            <span>El nombre debe tener al menos 3 caracteres.</span>
+          }
+        </div>
+      }
       <label>
         Email:
         <input type="email" formControlName="email" />
       </label>
+      @if (
+        formGroupContact.get('email')?.invalid &&
+        formGroupContact.get('email')?.touched
+      ) {
+        <div>
+          @if (formGroupContact.get('email')?.errors?.['required']) {
+            <span>El email es requerido.</span>
+          } @else if (formGroupContact.get('email')?.errors?.['email']) {
+            <span>El email debe tener el formato correcto.</span>
+          }
+        </div>
+      }
       <label>
         ¿Eres estudiante?
         <input type="checkbox" formControlName="isStudent" />
@@ -37,10 +66,20 @@ import { UserDTO } from '../../types.ts/user';
           <option value="noche">Noche</option>
         </select>
       </label>
-      <button type="submit" [disabled]="formGroupContact.invalid">Enviar</button>
+      <button type="submit" [disabled]="formGroupContact.invalid">
+        Enviar
+      </button>
     </form>
   `,
-  styles: ``,
+  styles: `
+    form {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      max-width: 400px;
+      margin: auto;
+    }
+  `,
 })
 export class ContactComponent {
   formGroupContact = new FormGroup({
